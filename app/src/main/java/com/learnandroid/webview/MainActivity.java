f123516b29e08b7be5499c97f92c61cbd3c43367
Malik Al Ichsan <malik.al.ichsan@gmail.com>
2018-01-10 12:32:13 +0700

    Webview loadData

--- app/src/main/java/com/learnandroid/webview/MainActivity.java
+++ app/src/main/java/com/learnandroid/webview/MainActivity.java
@@ -2,12 +2,16 @@ package com.learnandroid.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webview);
+        webView.loadData("<html><p>This is <strong>webview</strong></p></html>", "text/html", "UTF-8");
     }
 }
