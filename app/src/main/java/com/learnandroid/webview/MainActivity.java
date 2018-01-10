005b88e7a98f554dd660b88f0cb8f8e3605cc823
Malik Al Ichsan <malik.al.ichsan@gmail.com>
2018-01-10 12:43:58 +0700

    loadUrl dan javascript enabled

--- app/src/main/java/com/learnandroid/webview/MainActivity.java
+++ app/src/main/java/com/learnandroid/webview/MainActivity.java
@@ -2,16 +2,22 @@ package com.learnandroid.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
+    WebSettings webSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = (WebView)findViewById(R.id.webview);
-        webView.loadData("<html><p>This is <strong>webview</strong></p></html>", "text/html", "UTF-8");
+        webView = (WebView)findViewById(R.id.webview );
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        // webView.loadData("<html><p>This is <strong>webview</strong></p></html>", "text/html", "UTF-8");
+        webView.loadUrl("https://stackoverflow.com/");
     }
 }
